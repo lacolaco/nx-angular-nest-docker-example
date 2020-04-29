bf0083a39d2939593302adde80b822a74784ac8a
Suguru Inatomi <suguru.inatomi@gmail.com>
2020-04-29 18:54:40 +0900

    nestjs serve-static

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -8,7 +8,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello')
+  @Get('/api/hello')
   getData(): Message {
     return this.appService.getData();
   }
--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', 'client'),
+      exclude: ['/api*']
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
